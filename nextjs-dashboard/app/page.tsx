52ff102d4aaf4ab3d0abdcc10be1eaf90733e259
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2023-12-19 15:04:46 +0100

    chapter7: Fetching Data

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import {lusitania} from '@/app/ui/fonts';
+import {lusitana} from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
@@ -17,7 +17,8 @@ export default function Page() {
           className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
         /> */}
         <div className={styles.shape}></div>
-          <p className={`${lusitania.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <strong>tutorial midudev minuto 1:10 </strong>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
