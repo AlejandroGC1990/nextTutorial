8c5dc9d71d3c3214536adda54f625fdb869a58b6
AlexOscar <alejandro.garcia.carmona@gmail.com>
2023-12-27 20:34:35 +0100

    chapter 8: Static and Dynamic Rendering

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -18,7 +18,6 @@ export default function Page() {
         /> */}
         <div className={styles.shape}></div>
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-          <strong>tutorial midudev minuto 1:10 </strong>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
